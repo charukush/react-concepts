475da660470926eaa26b7edbfbc383b8b4f29bdd
Charu Kush <charu.kush@gmail.com>
2025-09-24 11:57:25 -0600

    edit

--- src/components/ListTest.jsx
+++ src/components/ListTest.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react';
 
 export default function ListTest() {
-    const [text, setText] = useState();
-    const [list, setList] = useState([]);
-    function handleList() {
-        setList([...list, text]);
-        setText(" ");
+  const [text, setText] = useState();
+  const [list, setList] = useState([]);
+  const [editText, setEditText] = useState(null);
+
+  function handleList() {
+    if (editText !==null) {
+      const updatedList = [...list];
+      updatedList[editText] = text;
+      setList(updatedList);
+      setEditText(null);
+    } else {
+      setList([...list, text]);
     }
-    function handleDelete(index) {
-        setList(list.filter((_, i)=> i !==index))
-    }
+    
+    setText(' ');
+  }
+  function handleDelete(index) {
+    setList(list.filter((_, i) => i !== index));
+  }
+
+  function handleEdit(index) {
+    setText(list[index]);
+    setEditText(index);
+  }
   return (
     <>
       <p>Test List!!</p>
@@ -19,12 +34,24 @@ export default function ListTest() {
         onChange={(e) => setText(e.target.value)}
       />
       <p>
-        <button className="btn-rounded" onClick={handleList}>Submit</button>
+        <button className="btn-rounded" onClick={handleList}>
+          {editText !== null ? 'Update' : 'Submit'}
+        </button>
       </p>
-          {list.map((items, index) => <li key={index}>
-              {items}
-              <button className = "btn-rounded btn-rounded-delete" onClick={()=>{handleDelete(index)}}>Delete</button>
-      </li>)}
+      {list.map((items, index) => (
+        <li key={index}>
+          {items}
+          <button className='btn-rounded' onClick={()=>handleEdit(index)}>Edit</button>
+          <button
+            className="btn-rounded btn-rounded-delete"
+            onClick={() => {
+              handleDelete(index);
+            }}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
     </>
   );
 }
